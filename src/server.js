2293bb28c70912b7671e7802d3df6430694a1db1
Siyabonga Samson Lukhele <samsonlukhele76@gmail.com>
2025-12-25 13:21:13 +0200

    chore: replace mongoose with prisma

--- src/server.js
+++ src/server.js
@@ -5,8 +5,6 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/basketball-stats";
 
 app.use(express.json());
 
